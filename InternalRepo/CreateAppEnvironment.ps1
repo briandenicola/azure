6df9d4e8b01fb55b4542a31a6264eba251fbdc46
Brian Denicola <bjd145@yahoo.com>
2016-08-12 08:10:00 -0500

    Updated CreateAppEnvironment.ps1 in Azure Automation.

--- InternalRepo/CreateAppEnvironment.ps1
+++ InternalRepo/CreateAppEnvironment.ps1
@@ -1,36 +1,33 @@
 param(
-	[string] $ResourceLocation = "southcentralus"
-    [string] $SubscriptionName,
-	[string] $VNet_IP_Range
+	[string] $ResourceLocation = "southcentralus",
+    [string] $SubscriptionName = "DevSub02",
+	[string] $VNet_IP_Range = "10.4.0.0/16", 
+	[string] $Mgmt_Subnet_Range = "10.4.200.0/24"
 )
-
-Import-Module Azure_functions
 
 $app_id    = Get-AutomationVariable -Name 'ApplicationId'
 $tenant_id = Get-AutomationVariable -Name 'TenantId' 
 $cert      = Get-AutomationCertificate -Name 'AuthCert'
 
-$CoreNetworkResourceGroup = "Core_Infra_Network_RG"
-$core_vnet_name  = "BJD-Core-VNet-001"
-$dns_server = "10.1.1.4"
-	
 if( !(Test-Path ("cert:\CurrentUser\My\{0}" -f $cert.Thumbprint) ) ) {
     $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("My", "CurrentUser") 
     $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
     $store.Add($cert) 
     $store.Close() 	
 }		
-
 Login-AzureRmAccount -ServicePrincipal -TenantId $tenant_id -CertificateThumbprint $cert.Thumbprint -ApplicationId $app_id 
 
-$network_resource_group = "{0}_Network_RG" -f $SubscriptionName
-New-AzureRmResourcegroup -Name $network_resource_group -Location $ResourceLocation -Verbose
-
+$CoreNetworkResourceGroup = "Core_Infra_Network_RG"
+$core_vnet_name  = "BJD-Core-VNet-001"
+$dns_server = "10.1.1.4"
 $VNet_name  = "{0}-VNet-001" -f $SubscriptionName
+$network_resource_group = "{0}_Network_RG" -f $SubscriptionName 
+
+New-AzureRmResourcegroup -Name $network_resource_group -Location $ResourceLocation
 
 $subnet_configs = @()
 $core_vnet_subnets = @(
-	@{ Name= 'Mgmt'; Range = "10.2.1.0/24" }
+	@{ Name= 'Mgmt'; Range = $Mgmt_Subnet_Range }
 )
 
 foreach( $subnet in $core_vnet_subnets) {
@@ -39,3 +36,8 @@ foreach( $subnet in $core_vnet_subnets) {
 
 New-AzureRmVirtualNetwork -Name $VNet_name -ResourceGroupName $network_resource_group -Location $ResourceLocation -AddressPrefix $VNet_IP_Range -Subnet $subnet_configs
 Set-AzureRMVnetDNSServer -ResourceGroupName $network_resource_group -VnetName $VNet_name -PrimaryDnsServerAddress $dns_server
+
+$core_vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $CoreNetworkResourceGroup -Name $core_vnet_name
+$depent_vnet = Get-AzureRmVirtualNetwork -ResourceGroupName $network_resource_group -Name $VNet_name
+Add-AzureRmVirtualNetworkPeering -name ("{0}-VNet" -f $VNet_name) -VirtualNetwork $core_vnet -RemoteVirtualNetworkId $depent_vnet.id -AllowForwardedTraffic -UseRemoteGateways 
+Add-AzureRmVirtualNetworkPeering -name "Core-VNet" -VirtualNetwork $depent_vnet -RemoteVirtualNetworkId $core_vnet.id -AllowForwardedTraffic -UseRemoteGateways
