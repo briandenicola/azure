a274163f0ae9bbe3912fe993ccd73f986601a421
Brian Denicola <bjd145@yahoo.com>
2017-03-14 14:38:44 -0500

    Updated StopAllVms.ps1 in Azure Automation.

--- InternalRepo/StopAllVms.ps1
+++ InternalRepo/StopAllVms.ps1
@@ -1,13 +1,3 @@
-$app_id    = Get-AutomationVariable -Name 'ApplicationId'
-$tenant_id = Get-AutomationVariable -Name 'TenantId' 
-$cert      = Get-AutomationCertificate -Name 'AuthCert'
-
-if( !(Test-Path ("cert:\CurrentUser\My\{0}" -f $cert.Thumbprint) ) ) {
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("My", "CurrentUser") 
-    $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-    $store.Add($cert) 
-    $store.Close() 	
-}		
-
-Login-AzureRmAccount -ServicePrincipal -TenantId $tenant_id -CertificateThumbprint $cert.Thumbprint -ApplicationId $app_id  
-Get-AzureRMVM | Stop-AzureRMVM -Force 
+$servicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"        
+Login-AzureRmAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+Get-AzureRMVM | Stop-AzureRMVM -Force
