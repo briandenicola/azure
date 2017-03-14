9d97a1d3abdd1da3d751acd88bc60d203d330fba
Brian Denicola <bjd145@yahoo.com>
2017-03-14 14:37:15 -0500

    Updated Set-AzurePolicyDefinitions.ps1 in Azure Automation.

--- InternalRepo/Set-AzurePolicyDefinitions.ps1
+++ InternalRepo/Set-AzurePolicyDefinitions.ps1
@@ -1,18 +1,8 @@
 [CmdletBinding()]
 param()
 
-$app_id    = Get-AutomationVariable    -Name 'ApplicationId'
-$tenant_id = Get-AutomationVariable    -Name 'TenantId' 
-$cert      = Get-AutomationCertificate -Name 'AuthCert'
-
-if( !(Test-Path ("cert:\CurrentUser\My\{0}" -f $cert.Thumbprint) ) ) {
-    $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("My", "CurrentUser") 
-    $store.Open([System.Security.Cryptography.X509Certificates.OpenFlags]::ReadWrite)
-    $store.Add($cert) 
-    $store.Close() 	
-}		
-
-Login-AzureRmAccount -ServicePrincipal -TenantId $tenant_id -CertificateThumbprint $cert.Thumbprint -ApplicationId $app_id
+$servicePrincipalConnection = Get-AutomationConnection -Name "AzureRunAsConnection"        
+Login-AzureRmAccount -ServicePrincipal -TenantId $servicePrincipalConnection.TenantId -ApplicationId $servicePrincipalConnection.ApplicationId -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
 
 $StorageResourceGroupName = "Core_Infra_Storage_RG"
 $ApplicationResourceGroupName = "DevSub01_App02_RG"
