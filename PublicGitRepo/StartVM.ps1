18909270a119294fbb861b1efb4bb75fb96e28d8
Brian Denicola <bjdmsdn@outlook.com>
2017-08-11 08:46:42 -0500

    New Code

--- PublicGitRepo/StartVM.ps1
+++
@@ -1,8 +0,0 @@
-$cred = Get-AutomationPSCredential -Name "AutomationCredential"
-
-$rgs = @("BJDRG")
-Add-AzureRmAccount -Credential $cred
-
-foreach( $group in $rgs) {
-    Get-AzureRMVM -ResourceGroupName $group | Start-AzureRMVM
-}
--- PublicGitRepo/StopVMs.ps1
+++
@@ -1,7 +0,0 @@
-$cred = Get-AutomationPSCredential -Name "AutomationCredential"
-Add-AzureRmAccount -Credential $cred
-
-$rgs = @("BJDRG")
-foreach( $group in $rgs) {
-    Get-AzureRMVM -ResourceGroupName $group | Stop-AzureRMVM
-}
