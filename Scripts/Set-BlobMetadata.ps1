5e34d332dcdd2f73a295921c6653d0ac216d4b78
Brian Denicola <bjd145@yahoo.com>
2018-10-30 16:12:52 -0500

    New Template for Storage Events

---
+++ Scripts/Set-BlobMetadata.ps1
@@ -0,0 +1,23 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string] $StorageName,
+    
+    [Parameter(Mandatory=$true)]
+    [string] $Storagekey,
+
+    [Parameter(Mandatory=$true)]
+    [string] $Container,
+
+
+    [Parameter(Mandatory=$true)]
+    [string] $FilePath
+)
+
+$ctx = New-AzureStorageContext -StorageAccountName $StorageName -StorageAccountKey $Storagekey
+$Metadata = @{
+    "Hash" = (Get-FileHash -Algorithm SHA256 -Path $FilePath).Hash
+}
+Set-AzureStorageBlobContent -File $FilePath -Container $Container -Metadata $Metadata -Context $ctx
+
+$blob = Get-AzureStorageBlob -Context $ctx -Container $Container -Blob $FilePath
+$blob.ICloudBlob.Metadata
