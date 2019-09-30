f1de3dd883e2a9bd36f580af374ef6910706ee9b
Brian Denicola <bjd145@yahoo.com>
2019-09-30 14:50:35 -0500

    Updated arg handling

--- Scripts/requestJITVMAccess.sh
+++ Scripts/requestJITVMAccess.sh
@@ -1,20 +1,56 @@
  #!/bin/bash 
  
-export vmName=$1
-export rgName=$2
-export port=$3
-export ipAddress=$4
+while (( "$#" )); do
+  case "$1" in
+    -n|--virtual-machine)
+      vmName=$2
+      shift 2
+      ;;
+    -g|--resource-group)
+      rgName=$2
+      shift 2
+      ;;
+    -s|--subscription-name)
+      subscriptionName=$2
+      shift 2
+      ;;
+    -p|--port)
+      port=$2
+      shift 2
+      ;;
+    -a|--ip-address)
+      ipAddress=$2
+      shift 2
+      ;;
+    -h|--help)
+      echo "Usage: ./requestJITVMAccess.sh -n {VM Name} -g {Resource Group} -s {Subscription} -p {22|3389} -a {IP Address or 0.0.0.0/0 for Any}"
+      exit 0
+      ;;
+    --) 
+      shift
+      break
+      ;;
+    -*|--*=) 
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+  esac
+done
 
-#az login 
+az account show  >> /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  az login 
+fi
+az account set -s ${subscriptionName}
 
 subid=`az account show -o tsv --query "id"`       
-listUri="https://management.azure.com/subscriptions/$subid/providers/Microsoft.Security/jitNetworkAccessPolicies?api-version=2015-06-01-preview"
+listUri="https://management.azure.com/subscriptions/${subid}/providers/Microsoft.Security/jitNetworkAccessPolicies?api-version=2015-06-01-preview"
 
-ascPolicy=`az rest -m GET --uri $listUri -o json --query "value"`
-ascName=`echo $ascPolicy | jq ".[0].name" | tr -d '"'`   
-ascLocation=`echo $ascPolicy | jq ".[0].location" | tr -d '"'`
+ascPolicy=`az rest -m GET --uri ${listUri} -o json --query "value"`
+ascName=`echo ${ascPolicy} | jq ".[0].name" | tr -d '"'`   
+ascLocation=`echo ${ascPolicy} | jq ".[0].location" | tr -d '"'`
 
-requestUri="https://management.azure.com/subscriptions/$subid/resourceGroups/$rgName/providers/Microsoft.Security/locations/$ascLocation/jitNetworkAccessPolicies/$ascName/initiate?api-version=2015-06-01-preview"
+requestUri="https://management.azure.com/subscriptions/${subid}/resourceGroups/${rgName}/providers/Microsoft.Security/locations/${ascLocation}/jitNetworkAccessPolicies/${ascName}/initiate?api-version=2015-06-01-preview"
 
 if [[ -z "${ipAddress}" ]]; then
   allowedIPAddress="0.0.0.0/0"
@@ -26,12 +62,12 @@ read -d '' requestBody << EOF
 {
   \"virtualMachines\": [
     {
-      \"id\": \"/subscriptions/$subid/resourceGroups/$rgName/providers/Microsoft.Compute/virtualMachines/$vmName\",
+      \"id\": \"/subscriptions/$subid/resourceGroups/${rgName}/providers/Microsoft.Compute/virtualMachines/${vmName}\",
       \"ports\": [
         {
-          \"number\": $port,
+          \"number\": ${port},
           \"duration\": \"PT3H\",
-          \"allowedSourceAddressPrefix": \"$allowedIPAddress\"
+          \"allowedSourceAddressPrefix": \"${allowedIPAddress}\"
         }
       ]
     }
@@ -39,4 +75,4 @@ read -d '' requestBody << EOF
 }
 EOF
 
-az rest -m post --uri $requestUri --body "$requestBody" --verbose 
+az rest -m post --uri ${requestUri} --body "${requestBody}" --verbose 
