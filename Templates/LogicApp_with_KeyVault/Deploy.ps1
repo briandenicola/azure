73d4f2d6eef9454cb0aea8305361fa7686d208d7
Brian Denicola <bjd145@yahoo.com>
2019-10-02 13:10:19 -0500

    Fixed Formatting

--- Templates/LogicApp_with_KeyVault/Deploy.ps1
+++ Templates/LogicApp_with_KeyVault/Deploy.ps1
@@ -1,8 +1,9 @@
 param (
-	[Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]       $ResourceGroupName,
-    [string]       $ResourceLocation           = "southcentralus"
+    [string]       $ResourceLocation = "southcentralus"
 )  
+
 $opts = @{
     Name                  = ("Deployment-{0}-{1}" -f $ResourceGroupName, $(Get-Date).ToString("yyyyMMddhhmmss"))
     ResourceGroupName     = $ResourceGroupName
@@ -10,8 +11,8 @@ $opts = @{
     TemplateParameterFile = (Join-Path -Path $PWD.Path -ChildPath "azuredeploy.parameters.json")
 }
 
-$account =  Get-AzContext | Select-Object -ExpandProperty Account 
+$account = Get-AzContext | Select-Object -ExpandProperty Account 
 $id = Get-AzADUser -UserPrincipalName $account.id | Select-Object -ExpandProperty Id 
 
 New-AzResourcegroup -Name $ResourceGroupName -Location $ResourceLocation -Verbose
-New-AzResourceGroupDeployment @opts -KeyVaultOwnerId $id -verbose   
+New-AzResourceGroupDeployment @opts -KeyVaultOwnerId $id -Verbose
