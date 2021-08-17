614b7472cd4cdbd357afa83297fb39d55cb897a2
Brian Denicola <brian@briandenicola.com>
2021-08-17 17:14:32 +0000

    Internal ASE not supported yet

--- Templates/AppServiceEnvironmentv3/main.tf
+++ Templates/AppServiceEnvironmentv3/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.12"
+  required_providers {
+    azurerm = "~> 2.60"
+  }
+}
+
+provider "azurerm" {
+  features  {}
+}
+
 data "azurerm_resource_group" "ase" {
   name = "DevSub01_ASEv3_RG"
 }
@@ -9,10 +20,10 @@ data "azurerm_subnet" "ase" {
 }
 
 resource "azurerm_app_service_environment_v3" "ase" {
-  name                          = "bjdasev3.1"
+  name                          = "bjdasev3-1"
   resource_group_name           = data.azurerm_resource_group.ase.name
   subnet_id                     = data.azurerm_subnet.ase.id
-  internal_load_balancing_mode  = "Web, Publishing"
+  //internal_load_balancing_mode  = "Web, Publishing" //https://github.com/hashicorp/terraform-provider-azurerm/issues/12251
 
   cluster_setting {
     name  = "DisableTls1.0"
