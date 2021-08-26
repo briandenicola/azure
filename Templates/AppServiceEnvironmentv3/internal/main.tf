5f29cf103b570638c8ad44fdc1e6a4d0412a1f4c
Brian Denicola <bjd145@yahoo.com>
2021-08-26 09:56:43 -0500

    Private Zone DNS

--- Templates/AppServiceEnvironmentv3/internal/main.tf
+++ Templates/AppServiceEnvironmentv3/internal/main.tf
@@ -1,3 +1,8 @@
+locals {
+  ase_name  = "bjdasev3-internal"
+  resource_group_name = "DevSub01_Network_RG"
+}
+
 terraform {
   required_version = ">= 0.13"
 
@@ -14,7 +19,12 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "ase" {
-  name = "DevSub01_ASEv3-internal_RG"
+  name = local.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet001" {
+  name                 = "DevSub01-VNet-001"
+  resource_group_name  = "DevSub01_Network_RG"
 }
 
 data "azurerm_subnet" "ase" {
@@ -23,8 +33,21 @@ data "azurerm_subnet" "ase" {
   resource_group_name  = "DevSub01_Network_RG"
 }
 
+resource "azurerm_private_dns_zone" "appserviceenvironment_net" {
+  name                      = "${local.ase_name}.appserviceenvironment.net"
+  resource_group_name       = data.azurerm_resource_group.ase.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "appserviceenvironment_net_link" {
+  name                  = "${local.ase_name}-link"
+  resource_group_name   = data.azurerm_resource_group.ase.name
+
+  private_dns_zone_name = azurerm_private_dns_zone.appserviceenvironment_net.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet001.id
+}
+
 resource "azurerm_app_service_environment_v3" "ase3" {
-  name                          = "bjdasev3-internal"
+  name                          = local.ase_name
   resource_group_name           = data.azurerm_resource_group.ase.name
   subnet_id                     = data.azurerm_subnet.ase.id
   internal_load_balancing_mode  = "Web, Publishing" 
