4debbfec945bb88bc5d2e6e7f2c31d39ed80086d
briandenicola <brian@briandenicola.com>
2023-04-24 10:40:18 -0500

    Removed unused variables

--- Templates/SimpleStorageDemo/FileCreation/variables.tf
+++ Templates/SimpleStorageDemo/FileCreation/variables.tf
@@ -3,16 +3,6 @@ variable "region" {
   default     =  "southcentralus"
 }
 
-variable "virtual_network_name" {
-  description = "The virtual network is for the storage account private endpoint"
-  default     = "DevSub02-Vnet-001"
-}
-
-variable "virtual_network_resource_group_name" {
-  description = "The Resource Group where the virtual network is deployed"
-  default     = "Apps02_Network_RG"
-}
-
 variable "storage_account_name" {
   description = "The virtual network is for the storage account private endpoint"
 }
