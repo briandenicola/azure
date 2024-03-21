58742b0e46061fdf4200865fab70bfdff38dde10
briandenicola <brian@briandenicola.com>
2024-03-21 15:34:04 -0500

    Restored network acls

--- Templates/AzureRedisCache/Enterprise-with-CMK/keyvault.tf
+++ Templates/AzureRedisCache/Enterprise-with-CMK/keyvault.tf
@@ -9,24 +9,30 @@ resource "azurerm_key_vault" "this" {
   enable_rbac_authorization   = true
   enabled_for_disk_encryption = true
 
-  sku_name                    = "standard"
+  sku_name = "standard"
+
+  # network_acls {
+  #   default_action = "Deny"
+  #   bypass         = "AzureServices"
+  #   ip_rules       = ["${chomp(data.http.myip.response_body)}/32"]
+  # }
 }
 
 resource "azurerm_private_endpoint" "kv" {
-  name                      = "${local.kv_name}-ep"
-  resource_group_name       = azurerm_resource_group.this.name
-  location                  = azurerm_resource_group.this.location
-  subnet_id                 = azurerm_subnet.private-endpoints.id
+  name                = "${local.kv_name}-ep"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  subnet_id           = azurerm_subnet.private-endpoints.id
 
   private_service_connection {
     name                           = "${local.kv_name}-ep"
     private_connection_resource_id = azurerm_key_vault.this.id
-    subresource_names              = [ "vault" ]
+    subresource_names              = ["vault"]
     is_manual_connection           = false
   }
 
   private_dns_zone_group {
-    name                          = azurerm_private_dns_zone.privatelink_vaultcore_azure_net.name
-    private_dns_zone_ids          = [ azurerm_private_dns_zone.privatelink_vaultcore_azure_net.id ]
+    name                 = azurerm_private_dns_zone.privatelink_vaultcore_azure_net.name
+    private_dns_zone_ids = [azurerm_private_dns_zone.privatelink_vaultcore_azure_net.id]
   }
 }
