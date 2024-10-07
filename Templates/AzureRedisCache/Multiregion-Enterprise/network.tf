82787f7e1dbe170c16c19a6fe5bef0c24c74fcab
briandenicola <brian@briandenicola.com>
2024-10-07 17:06:25 -0500

    🏏 Mon Oct  7 17:06:25 CDT 2024

--- Templates/AzureRedisCache/Multiregion-Enterprise/network.tf
+++ Templates/AzureRedisCache/Multiregion-Enterprise/network.tf
@@ -19,6 +19,14 @@ resource "azurerm_subnet" "private-endpoints" {
   address_prefixes     = [cidrsubnet(local.subnets[index(var.regions, each.key)], 8, 5)]
 }
 
+resource "azurerm_subnet" "compute" {
+  for_each             = local.regions_set
+  name                 = "compute"
+  resource_group_name  = azurerm_resource_group.this[each.key].name
+  virtual_network_name = azurerm_virtual_network.this[each.key].name
+  address_prefixes     = [cidrsubnet(local.subnets[index(var.regions, each.key)], 9, 5)]
+}
+
 resource "azurerm_network_security_group" "this" {
   for_each            = local.regions_set
   name                = "${local.resource_name}-${each.key}-nsg"
@@ -30,4 +38,24 @@ resource "azurerm_subnet_network_security_group_association" "this" {
   for_each                  = local.regions_set
   subnet_id                 = azurerm_subnet.private-endpoints[each.key].id
   network_security_group_id = azurerm_network_security_group.this[each.key].id
+}
+
+resource "azurerm_subnet_network_security_group_association" "compute" {
+  for_each                  = local.regions_set
+  subnet_id                 = azurerm_subnet.compute[each.key].id
+  network_security_group_id = azurerm_network_security_group.this[each.key].id
+}
+
+resource "azurerm_virtual_network_peering" "region_1_to_region_2" {
+  name                      = "region_1_to_region_2"
+  resource_group_name       = azurerm_resource_group.this[element(var.regions, 0)].name
+  virtual_network_name      = azurerm_virtual_network.this[element(var.regions, 0)].name
+  remote_virtual_network_id = azurerm_virtual_network.this[element(var.regions, 1)].id
+}
+
+resource "azurerm_virtual_network_peering" "region_2_to_region_1" {
+  name                      = "region_2_to_region_1"
+  resource_group_name       = azurerm_resource_group.this[element(var.regions, 1)].name
+  virtual_network_name      = azurerm_virtual_network.this[element(var.regions, 1)].name
+  remote_virtual_network_id = azurerm_virtual_network.this[element(var.regions, 0)].id
 }
--- Templates/AzureRedisCache/Multiregion-Enterprise/redis.tf
+++ Templates/AzureRedisCache/Multiregion-Enterprise/redis.tf
@@ -1,6 +1,6 @@
 resource "azurerm_redis_enterprise_cluster" "this" {
   for_each            = local.regions_set
-  name                = "${local.resource_name}-cache"
+  name                = "${local.resource_name}-${each.key}-cache"
   resource_group_name = azurerm_resource_group.this[each.key].name
   location            = each.key
   zones               = [1, 2, 3]
@@ -36,6 +36,22 @@ resource "azurerm_private_endpoint" "this" {
     name                 = azurerm_private_dns_zone.privatelink_redisenterprise_cache_azure_net[each.key].name
     private_dns_zone_ids = [azurerm_private_dns_zone.privatelink_redisenterprise_cache_azure_net[each.key].id]
   }
+}
+
+resource "azurerm_private_dns_a_record" "redis_region_1_in_region_2_dns" {
+  name                = "${azurerm_redis_enterprise_cluster.this[element(var.regions, 0)].name}.${element(var.regions, 0)}"
+  zone_name           = azurerm_private_dns_zone.privatelink_redisenterprise_cache_azure_net[element(var.regions, 1)].name
+  resource_group_name = azurerm_resource_group.this[element(var.regions, 1)].name
+  ttl                 = 300
+  records             = [ azurerm_private_endpoint.this[element(var.regions, 0)].private_service_connection[0].private_ip_address ]
+}
+
+resource "azurerm_private_dns_a_record" "redis_region_2_in_region_1_dns" {
+  name                = "${azurerm_redis_enterprise_cluster.this[element(var.regions, 1)].name}.${element(var.regions, 1)}"
+  zone_name           = azurerm_private_dns_zone.privatelink_redisenterprise_cache_azure_net[element(var.regions, 0)].name
+  resource_group_name = azurerm_resource_group.this[element(var.regions, 0)].name
+  ttl                 = 300
+  records             = [ azurerm_private_endpoint.this[element(var.regions, 1)].private_service_connection[0].private_ip_address ]
 }
 
 resource "azurerm_redis_enterprise_database" "this" {
