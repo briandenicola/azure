bac9021e129877e12fc8feeb77f215d57f442645
briandenicola <brian@briandenicola.com>
2024-10-09 10:11:13 -0500

    🏀 Wed Oct  9 10:11:13 CDT 2024

--- Templates/WindowsVM/bastion.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_bastion_host" "this" {
-  name                = local.bastion_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  sku                 = "Developer"
-  virtual_network_id  = azurerm_virtual_network.this.id 
-}
