a1269d164aafdcc803aca59e45addad88106a094
briandenicola <brian@briandenicola.com>
2024-10-14 15:47:21 -0500

    🏓 Mon Oct 14 15:47:21 CDT 2024

---
+++ Templates/LogicApp-Standard-AppService/infrastructure/workflow-roles.tf
@@ -0,0 +1,13 @@
+resource "azurerm_role_assignment" "test_storage_account_contributor_managed_identity" {
+  scope                            = azurerm_storage_account.test_account.id
+  role_definition_name             = "Storage Account Contributor"
+  principal_id                     = azurerm_user_assigned_identity.this.principal_id #azapi_resource.this.identity.0.principal_id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_role_assignment" "test_storage_data_reader_system_identity" {
+  scope                            = azurerm_storage_account.test_account.id
+  role_definition_name             = "Storage Blob Data Owner"
+  principal_id                     = azurerm_user_assigned_identity.this.principal_id #azapi_resource.this.identity.0.principal_id
+  skip_service_principal_aad_check = true
+}
