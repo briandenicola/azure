e82c98db87b313849d5fdf94c5de41c522378ded
briandenicola <brian@briandenicola.com>
2024-10-17 12:29:45 -0500

    🎲 Thu Oct 17 12:29:45 CDT 2024

--- Templates/LogicApp-Standard-ASEv3/ase.tf
+++
@@ -1,9 +0,0 @@
-data "azurerm_app_service_environment_v3" "this" {
-  name                = local.ase_name
-  resource_group_name = local.ase_rg_name
-}
-
-data "azurerm_service_plan" "app_service_plan_windows" {
-  name                = "${var.ase_app_name}-windows-hosting"
-  resource_group_name = local.ase_rg_name
-}
