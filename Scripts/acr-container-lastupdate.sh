28c9154faaff00f5ba6939d39bf2b52fc58b6203
briandenicola <brian@briandenicola.com>
2025-01-24 10:50:10 -0600

    🏉 Fri Jan 24 10:50:10 CST 2025

--- Scripts/acr-container-lastupdate.sh
+++ Scripts/acr-container-lastupdate.sh
@@ -21,16 +21,24 @@ while (( "$#" )); do
   esac
 done
 
+let now=`date +%s`
+
 # List all repositories in the ACR
 echo -e "\033[1m\e[38;5;45mListing all repositories in ACR: ${ACR_NAME}"
 repositories=$(az acr repository list --name ${ACR_NAME} --output tsv)
 
+printf "%-10s %-30s %-80s %-30s %-5s\n" "Registry" "Repo" "Tag" "Last Updated" "Age"
+printf "%-10s %-30s %-80s %-30s %-5s\n" "------" "----" "----" "---" "----"
+
 # Loop through each repository
 for repo in ${repositories}; do
-    manifestCount=$(az acr repository show --name ${ACR_NAME} --repository ${repo} --query manifestCount --output tsv)
-    echo -e "-----------------------------------------------------------------------------------------------------"
-    echo -e "🫙 Repo: ${repo} (${manifestCount} manifests)"
-    echo -e "-----------------------------------------------------------------------------------------------------"
-    az acr repository show-tags --name ${ACR_NAME} --repository ${repo} --query "[].{Tag: digest, LastUpdated: lastUpdateTime}" --output table --detail
-    echo -e " "
+    details=$(az acr repository show-tags --name ${ACR_NAME} --repository ${repo} --query "[].{Tag: digest, LastUpdated: lastUpdateTime}" --output json --detail)
+    
+    for tag in $(echo "${details}" | jq -r '.[].Tag'); do
+        last_updated=$(echo "${details}" | jq -r --arg tag "$tag" '.[] | select(.Tag == $tag) | .LastUpdated')
+        last_updated_epoch=$(date -d "$last_updated" +%s)
+        age=$(( (now - last_updated_epoch) / 86400 )) # Convert seconds to days
+
+        printf "%-10s %-30s %-80s %-30s %-5s\n" "${ACR_NAME}" "${repo}" "${tag}" "${last_updated}" "${age} days"
+    done
 done
