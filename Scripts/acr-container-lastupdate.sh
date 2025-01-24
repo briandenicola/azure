a10ce892e6f08794b3fa255c09efcbe296942d95
briandenicola <brian@briandenicola.com>
2025-01-24 11:09:42 -0600

    🎲 Fri Jan 24 11:09:42 CST 2025

--- Scripts/acr-container-lastupdate.sh
+++ Scripts/acr-container-lastupdate.sh
@@ -27,21 +27,23 @@ let now=`date +%s`
 echo -e "\033[1m\e[38;5;45mListing all repositories in ACR: ${ACR_NAME}"
 repositories=$(az acr repository list --name ${ACR_NAME} --output tsv)
 
-printf "%-10s %-30s %-75s %-20s %-10s %-30s %-5s\n" "Registry" "Repo" "Tag" "Type" "Size" "Last Updated" "Age"
+printf "%-10s %-30s %-75s %-20s %-10s %-30s %-5s\n" "Registry" "Repository" "SHA" "Type" "Size" "Last Updated" "Age"
 printf "%-10s %-30s %-75s %-20s %-10s %-30s %-5s\n" "------" "----" "----" "---" "----" "---" "----"
 
 # Loop through each repository
-for repo in ${repositories}; do
+for repo in ${repositories}; 
+do
     details=$(az acr manifest list-metadata --name ${repo} --registry ${ACR_NAME}  --query "[].{Tag: digest, LastUpdated: lastUpdateTime, Size: imageSize, Type: configMediaType}" --output json --only-show-errors)
     
-    for tag in $(echo "${details}" | jq -r '.[].Tag'); do
+    for tag in $(echo "${details}" | jq -r '.[].Tag'); 
+    do
         type=$(echo "${details}" | jq -r --arg tag "$tag" '.[] | select(.Tag == $tag) | .Type')
         size=$(echo "${details}" | jq -r --arg tag "$tag" '.[] | select(.Tag == $tag) | .Size')
         last_updated=$(echo "${details}" | jq -r --arg tag "$tag" '.[] | select(.Tag == $tag) | .LastUpdated')
         last_updated_epoch=$(date -d "$last_updated" +%s)
         age=$(( (now - last_updated_epoch) / 86400 )) # Convert seconds to days
-        type=$( echo "${type}" | cut -d'/' -f2 | cut -d'.' -f2-3)
-        size=$(numfmt --to=iec-i --suffix=B "${size}")
+        type=$( echo "${type}" | cut -d'/' -f2 | cut -d'.' -f2-3) #Get the last two parts of the string after splitting by '/' and '.'
+        size=$(numfmt --to=iec-i --suffix=B "${size}") #Format size
 
         printf "%-10s %-30s %-75s %-20s %-10s %-30s %-5s\n" "${ACR_NAME}" "${repo}" "${tag}" "${type}" "${size}" "${last_updated}" "${age} days"
     done
