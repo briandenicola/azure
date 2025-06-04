276ed2c0d7a4708138cf9188f8ac9db18c33ba5d
briandenicola <brian@briandenicola.com>
2025-06-04 10:21:07 -0500

    🙏 Wed Jun  4 10:21:07 CDT 2025

--- Templates/APIM/MCP/roles.tf
+++ Templates/APIM/MCP/roles.tf
@@ -7,5 +7,5 @@ resource "azurerm_role_assignment" "local_user_openai_user" {
 resource "azurerm_role_assignment" "apim_openai_user" {
   scope                            = module.openai.OPENAI_RESOURCE_ID
   role_definition_name             = "Cognitive Services OpenAI User"
-  principal_id                     = data.azurerm_client_config.current.object_id
+  principal_id                     = azurerm_user_assigned_identity.apim_identity.principal_id
 }
