3fe6bda3d3a8be997a356511eab4aa8768e20070
briandenicola <brian@briandenicola.com>
2025-08-06 13:35:05 -0500

    🏀 Wed Aug  6 13:35:05 CDT 2025

--- Templates/AzureRedisCache/AccessConfiguration/providers.tf
+++ Templates/AzureRedisCache/AccessConfiguration/providers.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.68.0"
+      version = "~> 4"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.7.0"
+      version = "~> 2"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.5.1"
+      version = "~> 3"
     }
   }
 }
--- Templates/AzureRedisCache/AccessConfiguration/redis.tf
+++ Templates/AzureRedisCache/AccessConfiguration/redis.tf
@@ -6,7 +6,7 @@ resource "azurerm_redis_cache" "this" {
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"
-  enable_non_ssl_port = false
+  non_ssl_port_enabled = false
   minimum_tls_version = "1.2"
 
   redis_configuration {
@@ -17,14 +17,14 @@ resource "azapi_update_resource" "redis" {
   type          = "Microsoft.Cache/Redis@2023-05-01-preview"
   resource_id   = azurerm_redis_cache.this.id
 
-  body = jsonencode({
+  body = {
     properties = {
       redisConfiguration = {
         maxmemory-policy = "volatile-lru"
         aad-enabled =  "True"
       }
     }
-  })
+  }
 }
 
 resource "azapi_resource" "access_policy_default" {
@@ -32,11 +32,11 @@ resource "azapi_resource" "access_policy_default" {
   name      = "default"
   parent_id = azurerm_redis_cache.this.id
 
-  body = jsonencode({
+  body = {
     properties = {
       permissions = "+@read allkeys"
     }
-  })
+  }
 }
 
 resource "azapi_resource" "access_policy_assignment" {
@@ -47,12 +47,12 @@ resource "azapi_resource" "access_policy_assignment" {
   name        = azurerm_user_assigned_identity.this.principal_id 
   parent_id   = azurerm_redis_cache.this.id
 
-  body = jsonencode({
+  body = {
     name = azurerm_user_assigned_identity.this.principal_id,
     properties = {
       accessPolicyName = "default",
       objectId = azurerm_user_assigned_identity.this.principal_id 
       objectIdAlias = local.identity_name
     }
-  })
+  }
 }
