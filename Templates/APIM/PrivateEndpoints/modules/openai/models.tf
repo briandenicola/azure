c3de1e4069480becbb60504df9c150419b6d5d4d
briandenicola <brian@briandenicola.com>
2025-08-12 08:11:26 -0500

    🏓 Tue Aug 12 08:11:26 CDT 2025

--- Templates/APIM/PrivateEndpoints/modules/openai/models.tf
+++ Templates/APIM/PrivateEndpoints/modules/openai/models.tf
@@ -2,12 +2,13 @@ resource "azurerm_cognitive_deployment" "gpt" {
   name                 = "gpt-4o"
   cognitive_account_id = azurerm_cognitive_account.this.id
   model {
-    format  = "OpenAI"
-    name    = "gpt-4o"
-    version = "2024-05-13"
+    format    = "OpenAI"
+    name      = "gpt-4o"
+    version   = "2024-11-20"
   }
 
   sku {
-    name = "Standard"
+    name     = "GlobalStandard"
+    capacity = 10
   }
 }
