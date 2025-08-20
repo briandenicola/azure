421528ec15f6cf212b349e313ce50a655b8f9c60
briandenicola <brian@briandenicola.com>
2025-08-20 08:42:37 -0500

    🥊 Wed Aug 20 08:42:37 CDT 2025

--- Templates/VirtualMachine/bastion.tf
+++ Templates/VirtualMachine/bastion.tf
@@ -1,4 +1,10 @@
 resource "azurerm_bastion_host" "this" {
+  depends_on = [ 
+    azurerm_subnet_network_security_group_association.compute,
+    azurerm_subnet_network_security_group_association.pe,
+    azurerm_nat_gateway.this,
+    azurerm_linux_virtual_machine.this
+  ]
   name                = local.bastion_name
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
