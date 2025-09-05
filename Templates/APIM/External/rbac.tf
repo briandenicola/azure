89f216cd133a0cbe24de7ac8ef6b48a8f2b5f2d9
briandenicola <brian@briandenicola.com>
2025-09-05 11:25:02 -0500

    🎮 Fri Sep  5 11:25:02 CDT 2025

--- Templates/APIM/External/rbac.tf
+++ Templates/APIM/External/rbac.tf
@@ -8,3 +8,15 @@ resource "azurerm_role_assignment" "apim_config_validator_rg_scoped" {
   principal_id                     = azurerm_api_management.this.identity[0].principal_id
   skip_service_principal_aad_check = true
 }
+
+resource "azurerm_role_assignment" "apim_config_reader" {
+  depends_on = [
+    azuread_service_principal.self_hosted,
+    azurerm_api_management.this,
+    azurerm_role_definition.apim_gateway_config_reader,
+  ]
+  scope                            = azurerm_api_management.this.id
+  role_definition_name             = azurerm_role_definition.apim_gateway_config_reader.name
+  principal_id                     = azuread_service_principal.self_hosted.object_id
+  skip_service_principal_aad_check = true
+}
